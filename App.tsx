51b20dc190c6fb1a0f8cace0524a188c461fb9f1
vboled <vboled@gmail.com>
2024-01-11 00:06:29 +0300

    Migrated to ts

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Calendar from './src/Calendar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Calendar/>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
