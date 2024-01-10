d63929019a324927bf45ff18e2555defe5f42205
vboled <vboled@gmail.com>
2024-01-11 00:16:53 +0300

    Added component

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+// import Calendar from './src/components/Calendar';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      {/* <Calendar/> */}
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+// AppRegistry.registerComponent('FrontendCale', () => App);
 
 const styles = StyleSheet.create({
   container: {
